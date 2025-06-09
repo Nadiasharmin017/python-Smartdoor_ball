d7f61156a33db01a5191841f830416e18750db51
Nadiasharmin017 <nadiasharmin017@gmail.com>
2025-06-09 11:19:42 +0600

    create face detection section

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import cv2
-cap = cv2.VideoCapture( )
+import face_recognition
+cap = cv2.VideoCapture(0)
+while True:
+    _, frame = cap.read()
+    faces = face_recognition.face_locations(frame)
+    print(faces)
+    cv2.imshow("Smart Doorball",frame)
+    # pressing the q key will close the window other wise it will be running infinite loop
+    if cv2.waitKey(10) == ord("q"):
+        break
+    # face recognition and face detection fornt on the cemera
